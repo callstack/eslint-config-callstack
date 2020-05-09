5d599b95324678389c13eb1a39582e02bd1b9b4c
Michał Pierzchała <thymikee@gmail.com>
2020-05-09 16:02:56 +0200

    expand test suite to cover extensions and expected errors

--- test/index.js
+++ test/index.js
@@ -1,11 +1,22 @@
 // @flow
 import React from 'react';
+import leftPad from 'left-pad'; // eslint-disable-line import/no-extraneous-dependencies, no-unused-vars
+// resolves JS extensions
+import Component from './Component'; // resolves .js
+import ComponentAndroid from './ComponentAndroid'; // resolves .android.js
+import ComponentIos from './ComponentIos'; // resolves .ios.js
+import ComponentNative from './ComponentNative'; // resolves .native.js
 
 type Props = {
   isTruthy: string,
 };
 
 export default class Bool extends React.Component<Props> {
+  Component = Component;
+  ComponentAndroid = ComponentAndroid;
+  ComponentIos = ComponentIos;
+  ComponentNative = ComponentNative;
+
   render() {
     return !!this.props.isTruthy;
   }
@@ -27,7 +38,8 @@ export function Hook() {
     } catch (_hugeObject) {
       // handled
     }
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <Bool />;
 }
--- test/index.tsx
+++ test/index.tsx
@@ -1,10 +1,31 @@
+// @flow
 import React from 'react';
+import leftPad from 'left-pad'; // eslint-disable-line import/no-extraneous-dependencies, @typescript-eslint/no-unused-vars
+// resolves JS extensions
+import Component from './Component'; // resolves .js
+import ComponentAndroid from './ComponentAndroid'; // resolves .android.js
+import ComponentIos from './ComponentIos'; // resolves .ios.js
+import ComponentNative from './ComponentNative'; // resolves .native.js
+// resolves TSX? extensions
+import ComponentTS from './ComponentTS'; // resolves .ts
+import ComponentAndroidTSX from './ComponentAndroidTSX'; // resolves .android.tsx
+import ComponentIosTS from './ComponentIosTS'; // resolves .ios.ts
+import ComponentNativeTSX from './ComponentNativeTSX'; // resolves .native.tsx
 
 type Props = {
   isTruthy: string;
 };
 
 export default class Bool extends React.Component<Props> {
+  Component = Component;
+  ComponentAndroid = ComponentAndroid;
+  ComponentIos = ComponentIos;
+  ComponentNative = ComponentNative;
+  ComponentTS = ComponentTS;
+  ComponentAndroidTSX = ComponentAndroidTSX;
+  ComponentIosTS = ComponentIosTS;
+  ComponentNativeTSX = ComponentNativeTSX;
+
   render() {
     return !!this.props.isTruthy;
   }
@@ -13,14 +34,21 @@ export default class Bool extends React.Component<Props> {
     let token = await this.current?.keepOwnership();
     return this.props.updateMode(token);
   };
+
+  animate = (_: ?number) => {};
 }
 
 export function Hook() {
   const map = new Map();
 
   React.useEffect(() => {
-    map.get('formData');
-  }, [map]);
+    try {
+      map.get('formData');
+    } catch (_hugeObject) {
+      // handled
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <Bool />;
 }
