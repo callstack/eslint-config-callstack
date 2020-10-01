93661cea5bc5da9ea9d4b192d935e915450fcf3b
Ferran Negre <fnp.developer@gmail.com>
2020-10-01 14:13:15 +0200

    Add browser env to react config (#160)
    * Add browser env to react config
    
    * Update react.js

--- react.js
+++ react.js
@@ -8,6 +8,9 @@ module.exports = {
     'plugin:react/recommended',
     'prettier/react',
   ],
+  env: {
+    browser: true,
+  },
   plugins: ['react', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
